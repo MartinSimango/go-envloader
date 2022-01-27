b54730281a09c06ecced3218f39ae3ee8ffc4ec4
Martin Simango <martin.simango@bsg.co.za>
2022-01-27 10:25:30 +0200

    Add support for float and bool env variables

--- environment.loader.impl.go
+++ environment.loader.impl.go
@@ -54,6 +54,54 @@ func (ecfp *EnvironmentLoaderImpl) LoadIntFromEnv(field string) (int, error) {
 	return intFieldValue, nil
 }
 
+func (ecfp *EnvironmentLoaderImpl) LoadFloatFromEnv(field string) (float64, error) {
+	envValue, err := ecfp.RegexParser.GetEnv(field)
+
+	if err != nil {
+		return 0, err
+	}
+
+	value, isEnv := ecfp.getEnv(*envValue)
+
+	if value == "" {
+		return 0, fmt.Errorf("error: could not find environment variable '%s'", envValue.Name)
+
+	}
+
+	floatFieldValue, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		if isEnv {
+			return 0, fmt.Errorf("error: failed to convert value '%s' to float. Environment variable %s = %s", value, envValue.Name, value)
+		}
+		return 0, fmt.Errorf("error: failed to convert string to int: %w", err)
+	}
+	return floatFieldValue, nil
+}
+
+func (ecfp *EnvironmentLoaderImpl) LoadBoolFromEnv(field string) (bool, error) {
+	envValue, err := ecfp.RegexParser.GetEnv(field)
+
+	if err != nil {
+		return false, err
+	}
+
+	value, isEnv := ecfp.getEnv(*envValue)
+
+	if value == "" {
+		return false, fmt.Errorf("error: could not find environment variable '%s'", envValue.Name)
+
+	}
+
+	boolFieldValue, err := strconv.ParseBool(value)
+	if err != nil {
+		if isEnv {
+			return false, fmt.Errorf("error: failed to convert value '%s' to int. Environment variable %s = %s", value, envValue.Name, value)
+		}
+		return false, fmt.Errorf("error: failed to convert string to int: %w", err)
+	}
+	return boolFieldValue, nil
+}
+
 func (ecfp *EnvironmentLoaderImpl) LoadStringFromEnv(field string) (string, error) {
 	envValue, err := ecfp.RegexParser.GetEnv(field)
 
